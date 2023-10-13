7d93094a709fd5c393d0227049b6300571b1b551
santiagoOrte <142599136+santiagoOrte@users.noreply.github.com>
2023-10-13 08:52:57 -0500

    MODIFICACION TALLER

--- src/veiculos/Automivil.java
+++ src/veiculos/Automivil.java
@@ -1,36 +1,43 @@
 package veiculos;
-public class Automivil extends Veiculo{
-    int numPuertas;
 
-    public Automivil(String marca, String modelo, String propietario, String placa, int año, int numPuertas) {
+public class Automovil extends Veiculo {
+    int numPuertas;
+    
+    //constructor
+    public Automovil(String marca, String modelo, String propietario, String placa, int año, int numPuertas) {
         super(marca, modelo, propietario, placa, año);
         this.numPuertas=numPuertas;
+        type="automovil";
     }
     
+    //getters
     public int getNumPuertas() {
         return numPuertas;
     }
+    public String getType() {
+        return type;
+    }
+    //setter
     public void setNumPuertas(int numPuertas) {
         this.numPuertas = numPuertas;
     }
     
+    //metodos   
     public int calcularAñosCirculacion (){
         int añosc;
         return añosc=2023-año;
     }
- 
+    
     @Override
     public void mostrarInformacion() {
         super.mostrarInformacion(); 
-        System.out.println("INFORMACION\n"
-                + "Marca:"+ marca 
-                + "Modelo:"+ modelo 
-                + "Propietario:"+ propietario 
-                + "Placa:" + placa 
-                + "Año de fabricacion:" + año 
-                + "Numero de puertas:" + numPuertas
-                + "años de circulacion:" + calcularAñosCirculacion());
+        System.out.println(""
+                + "|| Marca:"+ marca 
+                + "|| Modelo:"+ modelo 
+                + "|| Propietario:"+ propietario 
+                + "|| Placa:" + placa 
+                + "|| Año de fabricacion:" + año 
+                + "|| Numero de puertas:" + numPuertas
+                + "|| años de circulacion:" + calcularAñosCirculacion());
     }     
 }
-        
-
