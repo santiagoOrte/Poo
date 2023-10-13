d80af165e314082d57d6a3dc2485b902d56d999e
santiagoOrte <142599136+santiagoOrte@users.noreply.github.com>
2023-10-13 08:55:04 -0500

    MODIFICACION TALLER

--- src/veiculos/Motocicleta.java
+++ src/veiculos/Motocicleta.java
@@ -1,19 +1,29 @@
 package veiculos;
+
 public class Motocicleta extends Veiculo{
-    
+     
     double cilindrado;
+    
+    //cnstructor
     public Motocicleta(String marca, String modelo, String propietario, String placa, int año, double cilindrado) {
         super(marca, modelo, propietario, placa, año);
         this.cilindrado=cilindrado;
+        type="motocicleta";
     }
-
+    
+    //getters
     public double getCilindrado() {
         return cilindrado;
     }
+    public String getType() {
+        return type;
+    }
+    //setter
     public void setCilindrado(double cilindrado) {
         this.cilindrado = cilindrado;
     }
     
+    //metodos
     public int calcularAñosCirculacion (){
     int añosc;
     return añosc=2023-año;
@@ -22,14 +32,16 @@ public class Motocicleta extends Veiculo{
     @Override
     public void mostrarInformacion() {
         super.mostrarInformacion(); 
-        System.out.println("INFORMACION\n"
-                + "Marca:"+ marca 
-                + "Modelo:"+ modelo 
-                + "Propietario:"+ propietario 
-                + "Placa:" + placa 
-                + "Año de fabricacion:" + año 
-                + "Cilindraje:" + cilindrado
-                + "años de circulacion:" + calcularAñosCirculacion());
+        System.out.println(""
+                + "|| Marca:"+ marca 
+                + "|| Modelo:"+ modelo 
+                + "|| Propietario:"+ propietario 
+                + "|| Placa:" + placa 
+                + "|| Año de fabricacion:" + año 
+                + "|| Cilindraje:" + cilindrado
+                + "|| años de circulacion:" + calcularAñosCirculacion());
     } 
 
 }
+
+
